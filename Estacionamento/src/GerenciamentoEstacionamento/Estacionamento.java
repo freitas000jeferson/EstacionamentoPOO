fd2829126ab5168c490ae85ed4ba75c342a89e06
Matheus <matheusferreiradeoliveira21@gmail.com>
2019-04-13 13:08:36 -0300

    JOption OcuparVaga

--- Estacionamento/src/GerenciamentoEstacionamento/Estacionamento.java
+++ Estacionamento/src/GerenciamentoEstacionamento/Estacionamento.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
 import Connection.Conexao;
 
 public class Estacionamento {
@@ -15,7 +17,19 @@ public class Estacionamento {
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Erro: " + e.getMessage());
         }
-
+        //OCUPAR VAGA
+        String placa = JOptionPane.showInputDialog(null,
+				 "Informe a placa do ve�culo:",
+				 "Ocupar vaga", 
+				 JOptionPane.INFORMATION_MESSAGE);
+        int tipoVaga  = JOptionPane.showConfirmDialog(null,
+				"Necessita de uma vaga priorit�ria?", 
+				"Ocupar vaga",
+				JOptionPane.YES_NO_CANCEL_OPTION, 
+				JOptionPane.QUESTION_MESSAGE);
+        
+        
+        // 
 	}
 
 }
